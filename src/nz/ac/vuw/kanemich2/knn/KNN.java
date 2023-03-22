21ced3530bd8365245972f19b00e1666a5c5a5bd
Michael Kane <kanemich2@ecs.vuw.ac.nz>
2023-03-22 17:08:34 +1300

    Read file from first argument and display it
    First step in the process. Making sure errors such as missing
    file are handled appropriately

--- src/nz/ac/vuw/kanemich2/knn/KNN.java
+++ src/nz/ac/vuw/kanemich2/knn/KNN.java
@@ -1,15 +1,54 @@
 package nz.ac.vuw.kanemich2.knn;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class KNN {
-    public static void main(String[] args) {
-        System.out.print("Hello, ");
+    public KNN(String[] args) {
         if (args.length == 0) {
-            System.out.println("World!");
+            System.out.println("USAGE ass1-knn.jar <training-filename> <test-filename> <optional k-value>");
         } else {
-            for (int i = 0; i < args.length; i++) {
-                System.out.print(args[i] + " ");
+            List<String> trainingData = null;
+            try {
+                trainingData = readFile(args[0]);
+            } catch (FileNotFoundException e) {
+                System.err.println("Couldn't find training file. Make sure file is in same directory as the jar file. " +
+                        "Otherwise you need to include the entire filepath too.");
+                System.err.println("File \"" + args[0] + "\" not found.");
+                System.exit(0);
+            } catch (IOException e) {
+                System.out.println("Something went wrong reading the file.");
+                throw new RuntimeException(e);
             }
+
+            for (String data : trainingData) {
+                System.out.println(data);
+            }
+
         }
-        System.out.println();
+    }
+
+    private List<String> readFile(String fileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+        List<String> fileData = new ArrayList<>();
+        String line;
+        //Boolean isFirstline = true;
+        while ((line = br.readLine()) != null) {
+//            if (isFirstline) {
+//                // We want to ignore the first line as it all the labels
+//                isFirstline =  false;
+//            }
+            fileData.add(line);
+        }
+        return fileData;
+    }
+    public static void main(String[] args) {
+        KNN knn = new KNN(args);
     }
 }
+
+
