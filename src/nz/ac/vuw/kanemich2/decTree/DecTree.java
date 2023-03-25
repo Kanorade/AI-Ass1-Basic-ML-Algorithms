c5c763db5caf5a4879a9a589d58ee98dcf6b2126
Michael Kane <mike.james.kane@gmail.com>
2023-03-26 00:09:03 +1300

    Update file reading methods so they're more modular
    Separated the methods from the attributes in the original DataReader class basically.

--- src/nz/ac/vuw/kanemich2/decTree/DecTree.java
+++ src/nz/ac/vuw/kanemich2/decTree/DecTree.java
@@ -6,11 +6,11 @@ import java.io.IOException;
 import java.util.*;
 
 public class DecTree {
-    int numCategories;
-    int numAttributes;
-    Set<String> categoryNames;
-    List<String> attNames;
-    List<Instance> allInstances;
+    private record DataSet(int numCategories, int numAttributes, Set<String> categoryNames,
+                          List<String> attNames, List<Instance> allInstances){}
+    private DataSet trainingSet;
+    private DataSet testSet;
+
     public DecTree(String[] args) {
 
     }
@@ -18,9 +18,9 @@ public class DecTree {
     /**
      * Helper method copied over from the provided helper-code.java file  for
      * the assignment.
-     * @param fileName
+     * @param fileName The name of the file to be read
      */
-    private void readDataFile(String fileName) {
+    private DataSet readDataFile(String fileName) {
         /* format of names file:
          * names of attributes (the first one should be the class/category)
          * category followed by true's and false's for each instance
@@ -29,29 +29,30 @@ public class DecTree {
         try {
             Scanner din = new Scanner(new File(fileName));
 
-            attNames = new ArrayList<>();
+            List<String> attNames = new ArrayList<>();
             Scanner s = new Scanner(din.nextLine());
             // Skip the "Class" attribute.
             s.next();
             while (s.hasNext()) {
                 attNames.add(s.next());
             }
-            numAttributes = attNames.size();
+            int numAttributes = attNames.size();
             System.out.println(numAttributes + " attributes");
 
-            allInstances = readInstances(din);
+            List<Instance> allInstances = readInstances(din);
             din.close();
 
-            categoryNames = new HashSet<>();
+            Set<String> categoryNames = new HashSet<>();
             for (Instance i : allInstances) {
-                categoryNames.add(i.category);
+                categoryNames.add(i.getCategory());
             }
-            numCategories = categoryNames.size();
+            int numCategories = categoryNames.size();
             System.out.println(numCategories + " categories");
 
             for (Instance i : allInstances) {
                 System.out.println(i);
             }
+            return new DataSet(numCategories, numAttributes, categoryNames, attNames, allInstances);
         } catch (IOException e) {
             throw new RuntimeException("Data File caused IO exception");
         }
@@ -66,38 +67,39 @@ public class DecTree {
         System.out.println("Read " + instances.size() + " instances");
         return instances;
     }
-    private static class Instance {
 
-        private final String category;
-        private final List<Boolean> values;
-
-        public Instance(String cat, Scanner s) {
-            category = cat;
-            values = new ArrayList<>();
-            while (s.hasNextBoolean()) {
-                values.add(s.nextBoolean());
-            }
-        }
-
-        public boolean getAtt(int index) {
-            return values.get(index);
-        }
-
-        public String getCategory() {
-            return category;
-        }
-
-        public String toString() {
-            StringBuilder ans = new StringBuilder(category);
-            ans.append(" ");
-            for (Boolean val : values) {
-                ans.append(val ? "true " : "false ");
-            }
-            return ans.toString();
-        }
-
-    }
     public static void main(String[] args) {
         DecTree dt = new DecTree(args);
     }
+}
+ class Instance {
+
+    private final String category;
+    private final List<Boolean> values;
+
+    public Instance(String cat, Scanner s) {
+        category = cat;
+        values = new ArrayList<>();
+        while (s.hasNextBoolean()) {
+            values.add(s.nextBoolean());
+        }
+    }
+
+    public boolean getAtt(int index) {
+        return values.get(index);
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String toString() {
+        StringBuilder ans = new StringBuilder(category);
+        ans.append(" ");
+        for (Boolean val : values) {
+            ans.append(val ? "true " : "false ");
+        }
+        return ans.toString();
+    }
+
 }
